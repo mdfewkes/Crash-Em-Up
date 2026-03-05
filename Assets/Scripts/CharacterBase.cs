86dd7ca3f037afaa0c3b66e1a698588c6980dac1
Michael Fewkes <mdfewkes@gmail.com>
2026-03-04 22:01:45 -0800

    Created CharacterBaseClass and transitioned TestEnemy to child class

---
+++ Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,109 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using static UnityEngine.InputSystem.DefaultInputActions;
+
+public class CharacterBase : MonoBehaviour {
+
+	enum CharacterState { Move, Action, Stunned };
+	private CharacterState state = CharacterState.Move;
+
+	private bool iFrames = false;
+	private DamageArea[] damageAreas;
+	private Queue<ImpactData> impactDataQueue = new Queue<ImpactData>();
+	private bool pendingImpact = false;
+	private Vector2 impactVelocity = Vector2.zero;
+	public Vector2 knockbackMultiplier = new Vector2(1.0f, 1.0f);
+	float impactVelocitydamppening = 0.9f;
+
+	private Health healthComponent;
+
+	void Start() {
+		damageAreas = gameObject.GetComponentsInChildren<DamageArea>();
+		foreach (DamageArea damageArea in damageAreas) {
+			damageArea.OnCollision += ReceiveImpactData;
+		}
+
+		healthComponent = gameObject.GetComponent<Health>();
+		if (!healthComponent) healthComponent = gameObject.AddComponent<Health>();
+
+		OnStart();
+	}
+
+	protected virtual void OnStart() { }
+
+	private void OnDestroy() {
+		foreach (DamageArea damageArea in damageAreas) {
+			damageArea.OnCollision -= ReceiveImpactData;
+		}
+	}
+
+	void Update() {
+		if (iFrames) {
+			impactDataQueue.Clear();
+			pendingImpact = false;
+		}
+		if (pendingImpact) {
+			float highestMag = 0.0f;
+			while (impactDataQueue.Count > 0) {
+				ImpactData impactData = impactDataQueue.Dequeue();
+				impactVelocity += new Vector2(impactData.hitVelocity.x * knockbackMultiplier.x, impactData.hitVelocity.y * knockbackMultiplier.y);
+				impactVelocity += new Vector2(impactData.hitDirection.x * knockbackMultiplier.x, impactData.hitDirection.z * knockbackMultiplier.y);
+
+				float mag = impactVelocity.magnitude;
+				if (mag > highestMag) {
+					highestMag = mag;
+				}
+
+				state = CharacterState.Stunned;
+			}
+			impactVelocity.Normalize();
+			impactVelocity *= highestMag;
+
+			healthComponent.TakeDamage(1.0f);
+			pendingImpact = false;
+		}
+
+		switch (state) {
+		case CharacterState.Move:
+			MoveState();
+			break;
+		case CharacterState.Action:
+			ActionState();
+			break;
+		case CharacterState.Stunned:
+			StunnedState();
+			break;
+		default:
+			MoveState();
+			break;
+		}
+	}
+
+	protected virtual void MoveState() { }
+
+	protected virtual void ActionState() {
+		state = CharacterState.Move;
+	}
+
+	private void StunnedState() {
+		Vector3 newPosition = transform.position;
+		newPosition.x += impactVelocity.x;
+		newPosition.z += impactVelocity.y;
+		transform.position = newPosition;
+		impactVelocity *= impactVelocitydamppening;
+
+		if (impactVelocity.magnitude <= 0.1f) {
+			state = CharacterState.Move;
+			impactVelocity = Vector2.zero;
+			ExitStunnedState();
+		}
+	}
+
+	protected virtual void ExitStunnedState() { }
+
+	private void ReceiveImpactData(ImpactData impactData) {
+		impactDataQueue.Enqueue(impactData);
+		pendingImpact = true;
+	}
+}
--- Assets/Scripts/TestEnemy.cs
+++ Assets/Scripts/TestEnemy.cs
@@ -1,73 +1,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestEnemy : MonoBehaviour {
-	public Vector2 knockbackMultiplier = new Vector2(1.0f, 1.0f);
-	public float damppening = 0.9f;
-	Vector2 hitVelocity = Vector2.zero;
+public class TestEnemy : CharacterBase {
+	[SerializeField] float speed = 3.0f;
 
 	Vector3 startingPosition;
 
-	Health healthComponent;
-
-	private DamageArea[] damageAreas;
-	private Queue<ImpactData> impactDataQueue = new Queue<ImpactData>();
-	private bool pendingImpact = false;
-
-	void Start() {
-		damageAreas = gameObject.GetComponentsInChildren<DamageArea>();
-		foreach (DamageArea damageArea in damageAreas) {
-			damageArea.OnCollision += ReceiveImpactData;
-		}
-
+	protected override void OnStart() {
 		startingPosition = transform.position;
-		healthComponent = gameObject.GetComponent<Health>(); 
-		if (healthComponent == null ) healthComponent = gameObject.AddComponent<Health>();
 	}
 
-	private void OnDestroy() {
-		foreach (DamageArea damageArea in damageAreas) {
-			damageArea.OnCollision -= ReceiveImpactData;
+	protected override void MoveState() {
+		Vector3 pointToStart = startingPosition - transform.position;
+		if (pointToStart.magnitude > speed) {
+			pointToStart = pointToStart.normalized * speed;
 		}
-	}
-
-	void Update() {
-		if (pendingImpact) {
-			float highestMag = 0.0f;
-			while (impactDataQueue.Count > 0) {
-				ImpactData impactData = impactDataQueue.Dequeue();
-				hitVelocity += new Vector2(impactData.hitVelocity.x * knockbackMultiplier.x, impactData.hitVelocity.y * knockbackMultiplier.y);
-				hitVelocity += new Vector2(impactData.hitDirection.x * knockbackMultiplier.x, impactData.hitDirection.z * knockbackMultiplier.y);
-
-				float mag = hitVelocity.magnitude;
-				if (mag > highestMag) {
-					highestMag = mag;
-				}
-				hitVelocity.Normalize();
-				hitVelocity *= highestMag;
-
-				healthComponent.TakeDamage(1.0f);
-			}
-			pendingImpact = false;
-		} else if (hitVelocity.magnitude < 0.1f){
-			//Steer back to starting position
-			Vector3 pointToStart = startingPosition - transform.position;
-			if (pointToStart.magnitude > 1.0f)
-				pointToStart.Normalize();
-			Vector2 goHome = new Vector2(pointToStart.x, pointToStart.z) * Time.deltaTime;
-			hitVelocity += goHome;
-		}
-
+		Vector2 goHome = new Vector2(pointToStart.x, pointToStart.z) * Time.deltaTime;
 
 		Vector3 newPosition = transform.position;
-		newPosition.x += hitVelocity.x;
-		newPosition.z += hitVelocity.y;
+		newPosition.x += goHome.x;
+		newPosition.z += goHome.y;
 		transform.position = newPosition;
-		hitVelocity *= damppening;
-	}
-
-	private void ReceiveImpactData(ImpactData impactData) {
-		impactDataQueue.Enqueue(impactData);
-		pendingImpact = true;
 	}
 }
