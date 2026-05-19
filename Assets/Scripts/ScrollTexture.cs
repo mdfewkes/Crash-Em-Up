b4f9047cc5094178eeacbe74c59a0990468369a7
Christer Kaitila <breakdance@mcfunkypants.com>
2026-05-19 15:10:13 -0700

    stop git spam by avoiding unnecessary .mat file updates

--- Assets/Scripts/ScrollTexture.cs
+++ Assets/Scripts/ScrollTexture.cs
@@ -3,21 +3,35 @@ using UnityEngine;
 public class ScrollTexture : MonoBehaviour {
 	public Material material;
 	public Vector2 scrollSpeed;
+    private Vector2 startingOffset;
 
 	void Start() {
 		if (material == null) { 
-			material = GetComponent<Renderer>()?.material;
-		}
+            material = GetComponent<Renderer>()?.material;
+            startingOffset = material.mainTextureOffset;
+    	}
 		if (material == null) {
 			this.enabled = false;
 		}
+
 	}
 
 	void Update() {
-		Vector2 offset = material.mainTextureOffset;
+		if (!material) return;
+        Vector2 offset = material.mainTextureOffset;
 		offset = offset + scrollSpeed * Time.deltaTime;
 		offset = new Vector2 (offset.x % 1.0f, offset.y % 1.0f);
 
+        // note: this will force unity to
+        // update the .mat file on disk
 		material.mainTextureOffset = offset;
 	}
+
+    void onApplicationQuit()
+    {
+		if (!material) return;
+        // reset the offset to the previously saved value
+        // to avoid github spam .mat file updates every run
+        material.mainTextureOffset = startingOffset;
+    }
 }
