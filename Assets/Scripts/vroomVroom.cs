0cc450153b917d436a44e3076441e1c0cc347dee
Michael Fewkes <mdfewkes@gmail.com>
2026-05-24 18:35:23 -0700

    Vroomvroom can now infer motion from a target object

--- Assets/Scripts/vroomVroom.cs
+++ Assets/Scripts/vroomVroom.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class vroomVroom : MonoBehaviour
 {
+    [SerializeField] GameObject motionTarget;
+
     private AudioSource sfx;
     // start low for a nice revving sound to begin with
     private float currentPitch = 2.0f; 
@@ -9,24 +11,37 @@ public class vroomVroom : MonoBehaviour
     private float accelPitch = 3.0f;
     private float brakePitch = 0.5f;
     private float turnPitch = 3.0f;
-    private float pitchChangeSpeed = 1f;
+    private float pitchChangeSpeed = 2f;
+
+    private Vector3 lastPosition;
     
     void Start()
     {
         sfx = GetComponent<AudioSource>();
+
+        if (motionTarget) lastPosition = motionTarget.transform.position;
     }
 
     void Update()
     {
         if (!sfx) return;
+        if (!motionTarget) return;
+        Vector3 positionDelta = motionTarget.transform.position - lastPosition;
+        lastPosition = motionTarget.transform.position;
 
-        float accel = Input.GetAxis("Horizontal");
-        float turning = Input.GetAxis("Vertical");
+        float accel = positionDelta.x;
+        float turning = positionDelta.z;
 
         float targetPitch = idlePitch;
-        if (turning!=0f) targetPitch = turnPitch;
-        if (accel>0f) targetPitch = accelPitch;
-        if (accel<0f) targetPitch = brakePitch;
+        if (turning != 0f) {
+            targetPitch = turnPitch;
+        }
+        if (accel > 0f) {
+            targetPitch = accelPitch;
+        }
+        if (accel < 0f) {
+            targetPitch = brakePitch;
+        }
         
         currentPitch = Mathf.Lerp(currentPitch, targetPitch, pitchChangeSpeed * Time.deltaTime);
         sfx.pitch = currentPitch;
