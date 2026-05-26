7cee3d7c2a9c33839093551c9021c54a12f77538
Adeel Bashir <99957815+AdeelGameDev@users.noreply.github.com>
2026-05-26 12:09:21 +0500

    [WIP] PlayerCardUI

---
+++ Assets/Scripts/PlayerCardUI.cs
@@ -0,0 +1,8 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerCardUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI hitCounterText;
+    [SerializeField] private TextMeshProUGUI scoreText;
+}
