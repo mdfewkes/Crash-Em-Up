6d9c88ecca10c989c78141a51bcd3cf3753c9ee1
Michael Fewkes <mdfewkes@gmail.com>
2026-06-05 23:20:49 -0700

    Initial attack set for FastEnemy

--- Assets/Scripts/FastEnemy.cs
+++ Assets/Scripts/FastEnemy.cs
@@ -1,4 +1,36 @@
 using UnityEngine;
 
 public class FastEnemy : EnemyBase {
+	[SerializeField] private EnemyAction fuAttack;
+	[SerializeField] private EnemyAction fdAttack;
+	[SerializeField] private EnemyAction bAttack;
+	
+	private PlayerController player;
+
+	new void Start() {
+		base.Start();
+
+		player = FindAnyObjectByType<PlayerController>();
+	}
+
+	protected override void MoveState() {
+		base.MoveState();
+
+		FindAction();
+	}
+
+	private void FindAction() {
+		if (player == null) return;
+
+		Vector2 direction = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.z - transform.position.z).normalized;
+		float distance = Vector3.Distance(transform.position, player.transform.position);
+        float forwardProduct = Vector2.Dot(Vector2.right, direction);
+		float upProduct = Vector2.Dot(Vector2.up, direction);
+
+		if (distance < 3.0f) {
+            if (forwardProduct < -0.98 && distance <= 4.0f) RequestStartActionState(bAttack);
+            else if (forwardProduct > 0.0f && upProduct > 0.0f) RequestStartActionState(fuAttack);
+            else if (forwardProduct > 0.0f && upProduct < 0.0f) RequestStartActionState(fdAttack);
+		}
+	}
 }
