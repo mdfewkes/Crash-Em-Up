036c8c825e55cfc4f51d58a5adec55cf7fe06a4a
Michael Fewkes <mdfewkes@gmail.com>
2026-06-07 21:54:14 -0700

    Added jab to testenemy

--- Assets/Scripts/TestEnemy.cs
+++ Assets/Scripts/TestEnemy.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class TestEnemy : EnemyBase {
 	[SerializeField] private EnemyAction uAttack;
 	[SerializeField] private EnemyAction dAttack;
+	[SerializeField] private EnemyAction uJab;
+	[SerializeField] private EnemyAction dJab;
 
 	protected override void MoveState() {
 		base.MoveState();
@@ -15,11 +17,14 @@ public class TestEnemy : EnemyBase {
 
 		Vector2 direction = new Vector2(PlayerTag.playerTag.transform.position.x - transform.position.x, PlayerTag.playerTag.transform.position.z - transform.position.z).normalized;
 		float distance = Vector3.Distance(transform.position, PlayerTag.playerTag.transform.position);
+        float forwardProduct = Vector2.Dot(Vector2.right, direction);
 		float upProduct = Vector2.Dot(Vector2.up, direction);
 
 		if (distance < 3.0f) {
 			if (upProduct > 0.7f) RequestStartActionState(uAttack);
 			else if (upProduct < -0.7f) RequestStartActionState(dAttack);
+            else if (forwardProduct > 0.0f && upProduct > 0.0f && upProduct < 0.7f) RequestStartActionState(uJab);
+            else if (forwardProduct > 0.0f && upProduct < 0.0f && upProduct > -0.7f) RequestStartActionState(dJab);
 		}
 	}
 
